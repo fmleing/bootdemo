8709d9f92978a382d0a77d4d45da8e05bf36b971
fmleing <fmleing@163.com>
2017-10-25 10:51:00 +0800

    添加fastjsonBean

--- src/main/java/com/example/bootdemo/BootdemoApplication.java
+++ src/main/java/com/example/bootdemo/BootdemoApplication.java
@@ -1,12 +1,23 @@
 package com.example.bootdemo;
 
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
 
 @SpringBootApplication
 public class BootdemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootdemoApplication.class, args);
+	}
+
+	@Bean
+	public HttpMessageConverters fastJsonHttpMessageConverters() {
+		FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+		HttpMessageConverter<?> converter = fastConverter;
+		return new HttpMessageConverters(converter);
 	}
 }
